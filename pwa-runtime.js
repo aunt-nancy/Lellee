becfff5ce1aee1d48c4c070ea404463a53484760
aunt-nancy <Iamauntnancy@gmail.com>
2026-09-11 03:07:04 -0700

    Load internal Wave 1 Program Pack runtime

--- pwa-runtime.js
+++ pwa-runtime.js
@@ -114,3 +114,12 @@ document.addEventListener('lellee:pagechange',event=>{
 registerWorker();
 console.info('Lellee canonical PWA runtime 2026-09-03 loaded');
 })();
+
+(()=>{
+  if(document.querySelector('script[data-lellee-wave1-runtime]'))return;
+  const s=document.createElement('script');
+  s.src='/wave1-program-runtime.js?v=20260911-qa1';
+  s.async=false;
+  s.dataset.lelleeWave1Runtime='1';
+  document.head.appendChild(s);
+})();
